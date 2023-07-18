0065e65d0a7a7343b26489f7215d5a69657da84a
Aida <aidasaremi@gmail.com>
2023-07-18 16:01:00 +0200

    Make Portfolio list and Single Portfolio

--- src/context.js
+++ src/context.js
@@ -1,63 +1,159 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { useCallback } from 'react'
+// import React, { useState, useContext, useEffect } from 'react'
+// import { useCallback } from 'react'
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-const AppContext = React.createContext()
+// // const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+// const AppContext = React.createContext()
+
+// const AppProvider = ({ children }) => {
+
+//   // const [searchTerm, setSearchTerm] = useState('a')
+//   const [cocktails, setCocktails] = useState([
+//     {
+//       id: 1,
+//       name: 'Custom Cocktail 1',
+//       image: 'logo192.png',
+//       info: 'Custom cocktail description 1',
+//       glass: 'Custom glass type 1',
+//     },
+//     {
+//       id: 2,
+//       name: 'Custom Cocktail 2',
+//       image: 'path_to_image2.jpg',
+//       info: 'Custom cocktail description 2',
+//       glass: 'Custom glass type 2',
+//     },
+//   ]);
+//       const data = cocktails
+//       console.log(data);
+//       const { drinks } = data
+//           if (drinks) {
+//             const newCocktails = drinks.map((item) => {
+//               const {
+//                 idDrink,
+//                 strDrink,
+//                 strDrinkThumb,
+//                 strAlcoholic,
+//                 strGlass,
+//               } = item
+    
+//               return {
+//                 id: idDrink,
+//                 name: strDrink,
+//                 image: strDrinkThumb,
+//                 info: strAlcoholic,
+//                 glass: strGlass,
+//               }
+//             })
+//             setCocktails(newCocktails)
+//           } else {
+//             setCocktails([])
+//           }
+          
+       
+      
+
+
+//   // const fetchDrinks = useCallback( async () => {
+   
+//   //   try {
+//   //     const response = await fetch(`${url}${searchTerm}`)
+//   //     const data = await response.json()
+//   //     console.log(data);
+//   //     const { drinks } = data
+//   //     if (drinks) {
+//   //       const newCocktails = drinks.map((item) => {
+//   //         const {
+//   //           idDrink,
+//   //           strDrink,
+//   //           strDrinkThumb,
+//   //           strAlcoholic,
+//   //           strGlass,
+//   //         } = item
+
+//   //         return {
+//   //           id: idDrink,
+//   //           name: strDrink,
+//   //           image: strDrinkThumb,
+//   //           info: strAlcoholic,
+//   //           glass: strGlass,
+//   //         }
+//   //       })
+//   //       setCocktails(newCocktails)
+//   //     } else {
+//   //       setCocktails([])
+//   //     }
+      
+//   //   } catch (error) {
+//   //     console.log(error)
+      
+//   //   }
+//   // },[searchTerm])
+//   // useEffect(() => {
+//   //   fetchDrinks()
+//   // }, [searchTerm,fetchDrinks])
+//   return (
+//     <AppContext.Provider
+//       value={{  cocktails }}
+//     >
+//       {children}
+//     </AppContext.Provider>
+//   )
+//   }
+// // make sure use
+// export const useGlobalContext = () => {
+//   return useContext(AppContext)
+// }
+
+// export { AppContext, AppProvider }
+
+import React, { useState, useContext } from 'react';
+
+const customItems = [
+  {
+    id: 1,
+    image: '/images/image/restaurant Aida.png',
+    name : "Restaurant",
+    info :"family Restaurant",
+    category: "family Restaurant",
+    glass: "family Restaurant",
+    instructions :"family Restaurant",
+  
+    
+  },
+  {
+    id: 2,
+    image: '/images/image/GADA.co.png',
+    name : "design",
+    info :"designt",
+    category: "family Restaurant",
+    glass: "family Restaurant",
+    instructions :"family Restaurant",
+   
+    
+  },
+  // Add more items as needed
+];
+
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const [cocktails, setCocktails] = useState(customItems);
 
-  const [searchTerm, setSearchTerm] = useState('a')
-  const [cocktails, setCocktails] = useState([])
+  // Your other logic here...
 
-  const fetchDrinks = useCallback( async () => {
-   
-    try {
-      const response = await fetch(`${url}${searchTerm}`)
-      const data = await response.json()
-      console.log(data);
-      const { drinks } = data
-      if (drinks) {
-        const newCocktails = drinks.map((item) => {
-          const {
-            idDrink,
-            strDrink,
-            strDrinkThumb,
-            strAlcoholic,
-            strGlass,
-          } = item
-
-          return {
-            id: idDrink,
-            name: strDrink,
-            image: strDrinkThumb,
-            info: strAlcoholic,
-            glass: strGlass,
-          }
-        })
-        setCocktails(newCocktails)
-      } else {
-        setCocktails([])
-      }
-      
-    } catch (error) {
-      console.log(error)
-      
-    }
-  },[searchTerm])
-  useEffect(() => {
-    fetchDrinks()
-  }, [searchTerm,fetchDrinks])
   return (
-    <AppContext.Provider
-      value={{  cocktails, searchTerm, setSearchTerm }}
-    >
-      {children}
+    <AppContext.Provider value={{ cocktails }}>
+      {children}                               
+                                                 {/* the AppProvider acts as a parent component that provides data to its children components 
+                                                   through the AppContext.Provider. The AppContext.Provider is not a child itself, 
+                                                  but it wraps the children components to enable them to access the shared data from the context. */}
     </AppContext.Provider>
-  )
-}
-// make sure use
-export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  );
+};
 
-export { AppContext, AppProvider }
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
+
--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,61 +1,68 @@
 import React from 'react'
 
 import { useParams, Link } from 'react-router-dom'
+import { useGlobalContext } from '../context'
 
 export default function SingleCocktail() {
   const { id } = useParams()
- 
-  const [cocktail, setCocktail] = React.useState(null)
+  const { cocktails } = useGlobalContext();
 
-  React.useEffect(() => {
+  if (!cocktails) {
+    return <h2 className='section-title'>Loading...</h2>;
+  }
+  const cocktail = cocktails.find((item) => item.id === parseInt(id));
+ 
+  // const [cocktail, setCocktail] = React.useState(null)
+
+  // React.useEffect(() => {
    
-    async function getCocktail() {
-      try {
-        const response = await fetch(
-          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
-        )
-        const data = await response.json()
-        if (data.drinks) {
-          const {
-            strDrink: name,
-            strDrinkThumb: image,
-            strAlcoholic: info,
-            strCategory: category,
-            strGlass: glass,
-            strInstructions: instructions,
-            strIngredient1,
-            strIngredient2,
-            strIngredient3,
-            strIngredient4,
-            strIngredient5,
-          } = data.drinks[0]
-          const ingredients = [
-            strIngredient1,
-            strIngredient2,
-            strIngredient3,
-            strIngredient4,
-            strIngredient5,
-          ]
-          const newCocktail = {
-            name,
-            image,
-            info,
-            category,
-            glass,
-            instructions,
-            ingredients,
-          }
-          setCocktail(newCocktail)
-        } else {
-          setCocktail(null)
-        }
-      } catch (error) {
-        console.log(error)
-      }
+  //   async function getCocktail() {
+  //     try {
+  //       const response = await fetch(
+  //         `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+  //       )
+  //       const data = await response.json()
+  //       if (data.drinks) {
+  //         const {
+  //           strDrink: name,
+  //           strDrinkThumb: image,
+  //           strAlcoholic: info,
+  //           strCategory: category,
+  //           strGlass: glass,
+  //           strInstructions: instructions,
+  //           strIngredient1,
+  //           strIngredient2,
+  //           strIngredient3,
+  //           strIngredient4,
+  //           strIngredient5,
+  //         } = data.drinks[0]
+  //         const ingredients = [
+  //           strIngredient1,
+  //           strIngredient2,
+  //           strIngredient3,
+  //           strIngredient4,
+  //           strIngredient5,
+  //         ]
+  //         const newCocktail = {
+  //           name,
+  //           image,
+  //           info,
+  //           category,
+  //           glass,
+  //           instructions,
+  //           ingredients,
+  //         }
+  //         setCocktail(newCocktail)
+  //       } else {
+  //         setCocktail(null)
+  //       }
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
       
-    }
-    getCocktail()
-  }, [id])
+  //   }
+  //   getCocktail()
+  // }, [id])
  
   if (!cocktail) {
     return <h2 className='section-title'>no cocktail to display</h2>
@@ -67,7 +74,7 @@ export default function SingleCocktail() {
       info,
       glass,
       instructions,
-      ingredients,
+   
     } = cocktail
     return (
       <section className='section cocktail-section'>
@@ -93,15 +100,12 @@ export default function SingleCocktail() {
             <p>
               <span className='drink-data'>instructons :</span> {instructions}
             </p>
-            <p>
-              <span className='drink-data'>ingredients :</span>
-              {ingredients.map((item, index) => {
-                return item ? <span key={index}> {item}</span> : null
-              })}
-            </p>
+           
           </div>
         </div>
       </section>
     )
   }
 }
+
+
